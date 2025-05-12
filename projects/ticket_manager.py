e7f58bd4ac9172b5259805ff6889b5465da86e2c
Hamza Rashid <hamza.rashid@globalnorthstar.com>
2025-05-12 12:51:45 +0500

    Did migration of tickets from SS01 to SS02

--- projects/ticket_manager.py
+++ projects/ticket_manager.py
@@ -27,7 +27,7 @@ def modify_tickets():
         progress["percentage"] = 0
 
         # tickets = mantis.get_tickets_from_filter(config.get("GENERAL_ISSUES_FILTER_ID"))
-        tickets = [285939,413154,423574,408061,390269,412448,411940,415285,417068,420642,414984,412798,409331,402338,403417,421206,426600,417335,407931,418184,420635,420676,421634,417859,408350]
+        tickets = [438785,438382,438624,438415,438674,438672,438191,437510,438617,438615,438399,438422,438403,438380,438376,438295,438277,438276,437495,438219,437819,437307,437486,437138,437111,438100]
 
 
         if not tickets:
@@ -39,8 +39,9 @@ def modify_tickets():
         total_tickets = len(tickets)
         ticket_logger.info(f"Found {total_tickets} tickets to review")
 
-        CONTROL_TICKET= 436818
-        CONTROL_TICKET_DESC="MT#0436818: Nexus E5 (Re)Stability Sprint#1 : Retesting Control Ticket"
+        CONTROL_TICKET= 438810
+        CONTROL_TICKET_DESC="MT#0438810: Nexus E5 (Re)Stability Sprint#2 : Retesting Control Ticket"
+        OLD_CONTROL_TICKET = 436818
 
         for idx, ticket_data in enumerate(tickets):
             
@@ -50,13 +51,15 @@ def modify_tickets():
 
             # mantis.update_status_to_new(ticket_id=ticket_id)
 
-            # mantis.add_note_to_ticket(ticket_number=ticket_id,note_text=f"Relating the ticket to <b>{CONTROL_TICKET_DESC}</b> and marking the status back to <b>New</b>.")
+            mantis.add_note_to_ticket(ticket_number=ticket_id,note_text=f"Relating the ticket to <b>{CONTROL_TICKET_DESC}</b> and removing its link from <b>{OLD_CONTROL_TICKET}</b>.")
 
-            # mantis.relate_issues(ticket_id,CONTROL_TICKET)
+            mantis.relate_issues(ticket_id,CONTROL_TICKET)
+
+            mantis.unrelate_issues(ticket_id,OLD_CONTROL_TICKET)
 
             # mantis.update_qa_status_to_assigned(ticket_id=ticket_id)
 
-            mantis.update_owner(ticket_id=ticket_id,owner_id=StringConstants.SYED_KHURRAM_KAMRAN)
+            # mantis.update_owner(ticket_id=ticket_id,owner_id=StringConstants.SYED_KHURRAM_KAMRAN)
 
             ticket_logger.info(f"Updated the details for {mantis.get_ticket_url(ticket_id)}")
 
