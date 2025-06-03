914a0fe9f3fa352f7dec5588b2793eab62601c2d
Hamza Rashid <hamza.rashid@globalnorthstar.com>
2025-06-03 11:53:33 +0500

    Moving ticket to E06-B

--- projects/ticket_manager.py
+++ projects/ticket_manager.py
@@ -27,7 +27,7 @@ def modify_tickets():
         progress["percentage"] = 0
 
         # tickets = mantis.get_tickets_from_filter(config.get("GENERAL_ISSUES_FILTER_ID"))
-        tickets = [285939,405246,410315,412135,415292,415377,415579,415732,416028,416262,416755,416824,416825,417068,417112,417271,417291,417335,417600,417859,418039,418184,419179,419466,419908,419981,420010,420477,420514,420557,420642,420897,420939,421206,421288,421634,421776,422111,422121,422320,422406,423051,423193,423214,423219,423574,423799,423946,423969,424005,424027,424132,424172,424285,424528,424655,424946,424975,425669,425768,426208,426527,426535,426600,426934,427947,428113,428726,429439,429531,429739,430866,431426,434101,435105,435741,436417,436560,436581,436898,437159,437728,438381,438530]
+        tickets = [441297,440484,423103,441150,435163,439855,440160,440813,440811,434687,438191,439753,439087,438376,439604,439319,437510,437495,437111,437138,437486,437307,438219,438276,438277,438403,438422,438615]
 
 
         if not tickets:
@@ -39,39 +39,44 @@ def modify_tickets():
         total_tickets = len(tickets)
         ticket_logger.info(f"Found {total_tickets} tickets to review")
 
-        CONTROL_TICKET= 438810
-        CONTROL_TICKET_DESC="MT#0438810: Nexus E5 (Re)Stability Sprint#2 : Retesting Control Ticket"
-        OLD_CONTROL_TICKET = 436818
+        CONTROL_TICKET= 434669
+        CONTROL_TICKET_DESC="MT#0434669: Nexus E6-B: Regression Testing Control Ticket"
+        OLD_CONTROL_TICKET = 438810
 
         for idx, ticket_id in enumerate(tickets):
             
             
-            ticket_data = mantis.get_ticket_data(ticket_id)
-            ticket_history = ticket_data.get("history", [])
-
-            if mantis.has_attached_changeset(ticket_history):
-                ticket_logger.info(f"Changeset attached for NS70SS01 for ticket {mantis.get_ticket_url(ticket_id)}")
-                mantis.update_qa_status_to_accepted(ticket_id)
-                mantis.update_status_to_doh(ticket_id)
-                mantis.add_note_to_ticket(ticket_id,"Will be released in <b>Nexus E05</b> on the given ERD.")
-            else:
-                ticket_logger.info(f"No relevant changeset found for ticket {mantis.get_ticket_url(ticket_id)}")
+            # ticket_data = mantis.get_ticket_data(ticket_id)
+            
+            # ticket_history = ticket_data.get("history", [])
+            # if mantis.has_attached_changeset(ticket_history):
+            #     ticket_logger.info(f"Changeset attached for NS70SS01 for ticket {mantis.get_ticket_url(ticket_id)}")
+            #     mantis.update_qa_status_to_accepted(ticket_id)
+            #     mantis.update_status_to_doh(ticket_id)
+            #     mantis.add_note_to_ticket(ticket_id,"Will be released in <b>Nexus E05</b> on the given ERD.")
+            # else:
+            #     ticket_logger.info(f"No relevant changeset found for ticket {mantis.get_ticket_url(ticket_id)}")
+            # if mantis.has_status_changed(ticket_data,StringConstants.MANTIS_RESOLUTION_PARTIALLY_FIXED, 
+            #                              StringConstants.MANTIS_RESOLUTION_DOH, "hamzarashid"):
+            #     ticket_logger.info(f"Changed from partially fixed to DOH --> {mantis.get_ticket_url(ticket_id)}")
 
 
 
             # mantis.update_status_to_new(ticket_id=ticket_id)
 
-            # mantis.add_note_to_ticket(ticket_number=ticket_id,note_text=f"Relating the ticket to <b>{CONTROL_TICKET_DESC}</b> and removing its link from <b>{OLD_CONTROL_TICKET}</b>.")
+            mantis.add_note_to_ticket(ticket_number=ticket_id,note_text=f"Relating the ticket to <b>{CONTROL_TICKET_DESC}</b> and removing its link from <b>{OLD_CONTROL_TICKET}</b>.")
 
-            # mantis.relate_issues(ticket_id,CONTROL_TICKET)
+            mantis.relate_issues(ticket_id,CONTROL_TICKET)
 
-            # mantis.unrelate_issues(ticket_id,OLD_CONTROL_TICKET)
+            mantis.unrelate_issues(ticket_id,OLD_CONTROL_TICKET)
+
+            # mantis.update_status_to_new(ticket_id=ticket_id)
 
             # mantis.update_qa_status_to_assigned(ticket_id=ticket_id)
 
             # mantis.update_owner(ticket_id=ticket_id,owner_id=StringConstants.SYED_KHURRAM_KAMRAN)
 
-            # ticket_logger.info(f"Updated the details for {mantis.get_ticket_url(ticket_id)}")
+            ticket_logger.info(f"Updated the details for {mantis.get_ticket_url(ticket_id)}")
 
             
 
